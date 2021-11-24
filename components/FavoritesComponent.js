7da95ae0b540aeb1649133591d88d47300e9fff3
https://github.com/dweston38 <djweston38@gmail.com>
2021-11-24 12:38:24 -0500

    Animatable.

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -7,6 +7,7 @@ import { connect } from "react-redux";
 import { deleteFavorite } from "../redux/ActionCreators";
 import { baseUrl } from "../shared/baseUrl";
 import { Loading } from "./LoadingComponent";
+import * as Animatable from "react-native-animatable";
 
 const mapStateToProps = (state) => {
   return {
@@ -79,13 +80,15 @@ class Favorites extends Component {
       );
     }
     return (
-      <FlatList
-        data={this.props.campsites.campsites.filter((campsite) =>
-          this.props.favorites.includes(campsite.id)
-        )}
-        renderItem={renderFavoriteItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
+      <Animatable.View animation="fadeInRightBig" duration={2000}>
+        <FlatList
+          data={this.props.campsites.campsites.filter((campsite) =>
+            this.props.favorites.includes(campsite.id)
+          )}
+          renderItem={renderFavoriteItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </Animatable.View>
     );
   }
 }
